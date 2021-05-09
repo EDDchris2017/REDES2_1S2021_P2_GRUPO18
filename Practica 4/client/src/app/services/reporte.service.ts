d9924a69524004f4b0ffe7f13576c27c449f6dd0
Mario Morales <mmario_93@hotmail.es>
2021-05-09 09:36:04 -0600

    cambio de ip

--- Practica 4/client/src/app/services/reporte.service.ts
+++ Practica 4/client/src/app/services/reporte.service.ts
@@ -5,30 +5,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReporteService {
-  private API_URL: string = 'http://localhost:4000/'
+  private API_URL: string = 'http://18.116.53.157:4000/'
 
   constructor(private http: HttpClient) { }
 
   //Todo: Envio la informacion de los datos para crear reporte
   guardarReporte(carnet, nombre, curso, cuerpo) {
-    const data = {carnet, nombre, curso, cuerpo}
+    const data = { carnet, nombre, curso, cuerpo }
     //return [{ok:true, msg:'Reporte creado', carnet: '1111', nombre: 'nombre1', curso: 'redes2', cuerpo: 'hola ke ace', servidor: '201213283'}]
-    return this.http.post(`${this.API_URL}`,data);
+    return this.http.post(`${this.API_URL}`, data);
+  }
+
+  //Todo Recibo los datos del usuario
+  obtenerReportes() {
+    return this.http.get(`${this.API_URL}reporte/`)
   }
 
   //Todo: Envio la informacion de los datos para crear reporte de venetos
   guardarReporteEventos(carnet, nombre, nombreEvento, idEvento, extencion, base64) {
-    const data = {carnet, nombre, nombreEvento, idEvento, extencion, base64}
-    return this.http.post(`${this.API_URL}evento/`,data);
+    const data = { carnet, nombre, nombreEvento, idEvento, extencion, base64 }
+    return this.http.post(`${this.API_URL}evento/`, data);
   }
 
-  //Todo Recibo los datos del usuario
-  obtenerReportes(){
-    return this.http.get(`${this.API_URL}`)
-  }
-
-   //Todo Recibo los datos de la asistencia
-   obtenerReportesAsistencia(){
+  //Todo Recibo los datos de la asistencia
+  obtenerReportesAsistencia() {
     return this.http.get(`${this.API_URL}evento/`)
   }
 }
